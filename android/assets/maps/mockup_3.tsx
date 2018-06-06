be552bbbc4d00eb6ab6c767c831ffce1c4b69350
Edi-san <40005437+Edi-san@users.noreply.github.com>
2018-06-06 20:46:58 +0100

    Add files via upload

---
+++ android/assets/maps/mockup_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mockup_3" tilewidth="8" tileheight="8" tilecount="558" columns="31">
+ <image source="../tiles/mockup_3.png" width="250" height="149"/>
+</tileset>
---
+++ android/assets/tilesets/8x8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="8x8" tilewidth="8" tileheight="8" tilecount="544" columns="34">
+ <image source="../tiles/sheet_9.png" width="272" height="128"/>
+</tileset>

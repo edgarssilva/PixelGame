9c4ae7dc5b3fbfe0fe9d4e31130effb8e7c356aa
Edi-san <40005437+Edi-san@users.noreply.github.com>
2018-06-06 20:49:48 +0100

    Add files via upload

---
+++ android/android/assets/tilesets/packed_tiles.tsx
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_packed_1" tilewidth="16" tileheight="16">
+ <image source="../tiles/tiles_packed_1.png" width="368" height="128"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="-1"/>
+  <terrain name="Ground" tile="-1"/>
+ </terraintypes>
+ <tile id="6" terrain="1,1,1,"/>
+ <tile id="7" terrain="1,1,,"/>
+ <tile id="8" terrain="1,1,,1"/>
+ <tile id="20" terrain="0,0,0,"/>
+ <tile id="21" terrain="0,0,,"/>
+ <tile id="22" terrain="0,0,,0"/>
+ <tile id="24" terrain=",,,1"/>
+ <tile id="25" terrain=",,1,1"/>
+ <tile id="26" terrain=",,1,"/>
+ <tile id="27" terrain="1,1,1,1"/>
+ <tile id="29" terrain="1,,1,"/>
+ <tile id="31" terrain=",1,,1"/>
+ <tile id="43" terrain="0,,0,"/>
+ <tile id="45" terrain=",0,,0"/>
+ <tile id="47" terrain=",1,,1"/>
+ <tile id="49" terrain="1,,1,"/>
+ <tile id="52" terrain="1,,1,1"/>
+ <tile id="53" terrain=",,1,1"/>
+ <tile id="54" terrain=",1,1,1"/>
+ <tile id="66" terrain="0,,0,0"/>
+ <tile id="67" terrain=",,0,0"/>
+ <tile id="68" terrain=",0,0,0"/>
+ <tile id="70" terrain=",1,,"/>
+ <tile id="71" terrain="1,1,,"/>
+ <tile id="72" terrain="1,,,"/>
+ <tile id="89" terrain=",,,0"/>
+ <tile id="90" terrain=",,0,0"/>
+ <tile id="91" terrain=",,0,"/>
+ <tile id="112" terrain=",0,,0"/>
+ <tile id="113" terrain="0,0,0,0"/>
+ <tile id="114" terrain="0,,0,"/>
+ <tile id="135" terrain=",0,,"/>
+ <tile id="136" terrain="0,0,,"/>
+ <tile id="137" terrain="0,,,"/>
+</tileset>
---
+++ android/android/assets/tilesets/snow-expansion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snow-expansion" tilewidth="16" tileheight="16">
+ <image source="../tiles/snow-expansion.png" width="352" height="224"/>
+</tileset>
